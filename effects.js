90f1449b434714628dd24a78165c10d63ccdb170
ISHAN PAL <ishu4.op@gmail.com>
2025-06-28 20:37:46 +0530

    Update effects.js

--- effects.js
+++ effects.js
@@ -67,11 +67,3 @@ function endGame() {
   startBtn.disabled = false;
 }
 
-const leftBtn = document.getElementById('left-btn');
-const rightBtn = document.getElementById('right-btn');
-
-leftBtn.addEventListener('touchstart', () => player.dx = -player.speed);
-leftBtn.addEventListener('touchend', () => player.dx = 0);
-
-rightBtn.addEventListener('touchstart', () => player.dx = player.speed);
-rightBtn.addEventListener('touchend', () => player.dx = 0);
